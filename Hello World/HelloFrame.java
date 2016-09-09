518fe799204d9bbef2ad89459ad1574d26157add
Jason32A <jacksondoe1@hotmail.com>
2016-09-09 12:41:46 -0400

    APCS 9/9/16
    What we did September 9th

---
+++ Hello World/HelloFrame.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class HelloFrame here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JFrame;
+
+public class HelloFrame
+{
+   public static void main (String[] args){
+       JFrame FrametheQuestion = new JFrame();
+       FrametheQuestion.setTitle("Hello World");
+       FrametheQuestion.setSize(300,500);
+       FrametheQuestion.setVisible(true);
+
+    }
+}
---
+++ Hello World/Sup.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Sup here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sup
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
