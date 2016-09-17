71a9ab7694ca8c4c1aa814a8c17213dc38e3c9bb
Jason32A <jacksondoe1@hotmail.com>
2016-09-17 19:54:15 -0400

    9/17/16 sum of all work

---
+++ Testing/CharTest.java
@@ -0,0 +1,10 @@
+
+
+public class CharTest
+{
+   public static void main(){
+   for (int i = 0; i < 256;i++){
+   System.out.print((char)i);
+}
+}
+}
---
+++ Testing/StringTest.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class StringTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StringTest
+{
+    String testing;
+    public void main(String[] args)
+    {
+        testing = "45";
+        int testing2 = Integer.parseInt(testing);
+        System.out.print(testing2);
+    }
+}
